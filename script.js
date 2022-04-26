c1991a9c9b5bee3b3f85c18196b6445c746380aa
ravencancio <102492480+arcan9@users.noreply.github.com>
2022-04-25 20:14:28 -0700

    Add more event listeners; change info on click
    clicking each individual item changes the image & text

--- script.js
+++ script.js
@@ -1,8 +1,35 @@
 function changeDragon() {
-  let sprite = document.querySelector("#sprite");
-  // sprite.innerHTML = '<img src="src/dragon-solid.svg" alt="dragon"/>';
-  sprite.src = "src/dragon-solid.svg";
+  document.querySelector("#sprite").src = "src/dragon-solid.svg";
+  document.querySelector("#lorem").innerHTML =
+    "The fruit turned the sprite into a dragon!";
 }
 
 let fruit = document.querySelector("#fruit");
 fruit.addEventListener("click", changeDragon);
+
+function changeCrow() {
+  document.querySelector("#sprite").src = "src/crow-solid.svg";
+  document.querySelector("#lorem").innerHTML =
+    "The mushroom turned the sprite into a crow!";
+}
+
+let mushroom = document.querySelector("#mushroom");
+mushroom.addEventListener("click", changeCrow);
+
+function changeFrog() {
+  document.querySelector("#sprite").src = "src/frog-solid.svg";
+  document.querySelector("#lorem").innerHTML =
+    "The leaf turned the sprite into a frog!";
+}
+
+let leaf = document.querySelector("#leaf");
+leaf.addEventListener("click", changeFrog);
+
+function changeHuman() {
+  document.querySelector("#sprite").src = "src/man-people-svgrepo-com.svg";
+  document.querySelector("#lorem").innerHTML =
+    "The alcohol turned the sprite into a human! You turned him back into his true form. Congrats! He is incredibly grateful!";
+}
+
+let alcohol = document.querySelector("#alcohol");
+alcohol.addEventListener("click", changeHuman);
